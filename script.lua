68b9d6670590943051b06c558373a74bfa8a40ef
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-12 18:48:36 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,7 +7,7 @@ for _, v in ReplicatedStorage:GetDescendants() do
 		if v.Name == "RemoveItem" then
 			v:InvokeServer("Gregs Will", -50)
 		elseif v.Name == "UpdateDualWeapon" then
-			v:InvokeServer("Muramusa")
+			v:InvokeServer("Gregs Will")
 		-- elseif v.Name == "Trade" then
 		-- 	for i = 1, 2 do
 		-- 		v:InvokeServer("InitiateTrade", Players["xLetmesoloherx"])
