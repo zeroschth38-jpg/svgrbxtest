155d71c94fa8a56b753aa114c4e86d60f4a3eb1c
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-12 09:15:26 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,7 +5,7 @@ for _, v in ReplicatedStorage:GetDescendants() do
 	if v:IsA("RemoteFunction") then
 		-- print(v, "RMFN")
 		if v.Name == "RemoveItem" then
-			-- v:InvokeServer("Gregs Will", 0-(-50))
+			v:InvokeServer("Gregs Will", -50)
 		elseif v.Name == "UpdateDualWeapon" then
 			v:InvokeServer("Muramusa")
 		end
