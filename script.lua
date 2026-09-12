65dce76b1562dbd64312f0196ae2721461c35708
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-12 10:08:36 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,17 @@
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+-- local Players = game:GetService("Players")
+-- local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-for _, v in ReplicatedStorage:GetDescendants() do
-	if v:IsA("RemoteFunction") then
-		-- print(v, "RMFN")
-		if v.Name == "RemoveItem" then
-			v:InvokeServer("Gregs Will", -50)
-		elseif v.Name == "UpdateDualWeapon" then
-			v:InvokeServer("Muramusa")
-		end
-	elseif v:IsA("RemoteEvent") then
-		-- print(v, "RMEV")
-	end
-end
+-- for _, v in ReplicatedStorage:GetDescendants() do
+-- 	if v:IsA("RemoteFunction") then
+-- 		-- print(v, "RMFN")
+-- 		if v.Name == "RemoveItem" then
+-- 			v:InvokeServer("Gregs Will", -50)
+-- 		elseif v.Name == "UpdateDualWeapon" then
+-- 			v:InvokeServer("Muramusa")
+-- 		end
+-- 	elseif v:IsA("RemoteEvent") then
+-- 		-- print(v, "RMEV")
+-- 	end
+-- end
 
-print("Equipping", Players.LocalPlayer.PlayerStats.DualWeapon.Value)
+-- print("Equipping", Players.LocalPlayer.PlayerStats.DualWeapon.Value)
