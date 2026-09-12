5bb98d8f8a8fe4714959bd2f332224dd2230e8c2
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-12 07:22:15 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,13 +2,13 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 for _, v in ReplicatedStorage:GetDescendants() do
 	if v:IsA("RemoteFunction") then
-		print(v, "RMFN")
+		-- print(v, "RMFN")
 		if v.Name == "RemoveItem" then
 			v:InvokeServer("Gregs Will", -50)
 		elseif v.Name == "UpdateDualWeapon" then
 			v:InvokeServer("Gregs Will")
 		end
 	elseif v:IsA("RemoteEvent") then
-		print(v, "RMEV")
+		-- print(v, "RMEV")
 	end
 end
