99140149692c9a06baa6750b23632ee366693858
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-15 10:22:12 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -289,7 +289,7 @@ local function updatePosition()
 		local Position = RootPart.Position
 
 		PositionLabel.Text = string.format(
-			"Position   X %.1f   Y %.1f   Z %.1f",
+			"Position XYZ   %.1f,   %.1f,   %.1f",
 			Position.X,
 			Position.Y,
 			Position.Z
