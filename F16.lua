f64daf66170c08299d4d547c632aa20f227e1c8e
zeroschth38-jpg <zeroschth38@gmail.com>
2026-09-16 15:27:54 +0700

    Update F16.lua

--- F16.lua
+++ F16.lua
@@ -20,6 +20,9 @@ local Targets = {
 	Vector3.new(-1364, 172, 1962),
 	Vector3.new(-1363, 174, 2068),
 	Vector3.new(-1437, 176, 2494),
+	Vector3.new(-1402, 175, 2524),
+	Vector3.new(-1654, 174, 2619),
+	Vector3.new(-1792, 175, 2769),
 }
 
 local Character
@@ -36,7 +39,7 @@ local TargetCurrency = "Golden Shell"
 local LastInventory  = nil
 local EventCurrency  = 0
 
-local VERSION = "v0.5"
+local VERSION = "v0.51"
 
 local TargetPlaceID  = 11987539001
 local MAX_SERVER_AGE = 8 * 60 * 60
